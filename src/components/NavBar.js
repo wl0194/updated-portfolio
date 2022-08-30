4c3a798fa6ba0e64b36b92807fc832ca62a0a13c
Amy Hwang <728amyh@gmail.com>
2022-08-30 14:33:02 -0400

    set up navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,52 @@
 
 import { NavBar, Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState('false');
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+        window.addEventListener("scroll, onScroll");
+
+        return () => window.removeEventListener("scroll, onScroll");
+    }, [])
+
 
     return (
-        <Navbar expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""} >
+            < Container >
+                <Nav href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Home</Nav
                     <img src={''} alt="Logo" />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" >
-                    <span className="navbar-toggler-icon"> </span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#intro">AboutMe</Nav.Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" >
+                <span className="navbar-toggler-icon"> </span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#intro">About</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
 
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+
+                </Nav>
+                <span className="navbar-text">
+                    <div className="social-icons">
+                        <a href="#"><img src={ } alt="" /></a>
+                        <a href="#"><img src={ } alt="" /></a>
+                        <a href="#"><img src={ } alt="" /></a>
+                    </div>
+                    <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+                </span>
+            </Navbar.Collapse>
+            </ >
         </Navbar >
     );
 }
