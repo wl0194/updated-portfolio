50909dc621015957f5db8cf014b7cbb20f1c8ee7
Amy Hwang <728amyh@gmail.com>
2022-08-30 18:06:05 -0400

    imported nav icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,13 @@
 
 import { NavBar, Container } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import { FaLinkedinIn } from "react-icons/fa";
+import { AiOutlineDownload } from "react-icons/ai";
+import pdf from "../../assets/img/developer_resume.pdf";
+import {
+    AiFillGithub,
+} from "react-icons/ai";
+
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -19,33 +26,36 @@ export const NavBar = () => {
         return () => window.removeEventListener("scroll, onScroll");
     }, [])
 
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
 
     return (
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""} >
             < Container >
-                <Nav href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Home</Nav
-                    <img src={''} alt="Logo" />
+                <Navbar.Brand href="#home">
                 </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" >
-                <span className="navbar-toggler-icon"> </span>
-            </Navbar.Toggle>
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#intro">About</Nav.Link>
-                    <Nav.Link href="#projects">Projects</Nav.Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" >
+                    <span className="navbar-toggler-icon"> </span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active-navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#intro" className={activeLink === 'intro' ? 'active-navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('intro')}>About</Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active-navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
 
 
-                </Nav>
-                <span className="navbar-text">
-                    <div className="social-icons">
-                        <a href="#"><img src={ } alt="" /></a>
-                        <a href="#"><img src={ } alt="" /></a>
-                        <a href="#"><img src={ } alt="" /></a>
-                    </div>
-                    <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
-                </span>
-            </Navbar.Collapse>
+                    </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icons">
+                            <a href="https://www.linkedin.com/in/amy-hwang-633194216/"><img src={FaLinkedinIn} alt="" /></a>
+                            <a href="https://github.com/wl0194"><img src={AiFillGithub} alt="" /></a>
+                            <a href={pdf}><img src={AiOutlineDownload} alt="" /></a>
+                        </div>
+                        <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+                    </span>
+                </Navbar.Collapse>
             </ >
         </Navbar >
     );
