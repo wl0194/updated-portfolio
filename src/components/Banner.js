64bbc2b783945846dd7ecc67bed173cfda2dc692
Amy Hwang <728amyh@gmail.com>
2022-09-07 17:31:31 -0400

    changed color for button and wording on banner section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,7 +12,7 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Oh, hello there</span>
-                        <h1>{`Welcome to Amy Hwang's portfolio.`}</h1>
+                        <h1>{`My name is Amy J Hwang.`}</h1>
                         <p>I'm a graduate from a full stack bootcamp offered through University of Pennsylvania.
                             From the moment I produced "Hello World" in the console of my first application, I knew I was hooked into the world of software development. It has offered an engaging challenge to continually learn and improve any skills in creating high-quality software.<br></br><br></br>
                             I'm a UI focused developer who is not afraid of acquiring new skills and loves tackling new challenges
